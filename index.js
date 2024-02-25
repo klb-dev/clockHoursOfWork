46dfe7f4a6d577a24ce0e288c5667ec3505da349
Karen Byrd <klbdev88@gmail.com>
2024-02-25 11:14:29 -0600

    loading first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const btnStopAnimation = document.getElementById('btn-stop-animation');
+const formContainer = document.getElementById('form-container');
+const questionMark = document.getElementById('question-mark');
+
+btnStopAnimation.addEventListener('click', () => {
+  document.getElementById('alarm-clock').style.animation = 'none';
+});
+
+
+
+
+
